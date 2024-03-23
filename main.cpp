589222823c7ecfbd8d9e8b2f76633c4561b2dee9
최준형 <k14414@naver.com>
2024-03-24 00:12:42 +0900

    게임 벽 생성

--- main.cpp
+++ main.cpp
@@ -3,8 +3,74 @@
 
 #include "console.h"
 
+#define BOARD_SIZE 20
+#define MOVE_DELAY 60
+#define WALL_VERTICAL_STRING "┃"
+#define WALL_HORIZONTAL_STRING "━"
+#define WALL_RIGHT_TOP_STRING "┓"
+#define WALL_LEFT_TOP_STRING "┏"
+#define WALL_RIGHT_BOTTOM_STRING "┛"
+#define WALL_LEFT_BOTTOM_STRING "┗"
+#define SNAKE_STRING "■"
+#define SNAKE_BODY_STRING "■"
+#define APPLE_STRING "●"
+
+int x = 0;
+int y = 0;
+
+void game();
+void drawWall();
 int main()
 {
+    
+    game();
+}
 
+void game()
+{
+    console::init();
 
+    while(true)
+    {
+        console::clear();
+        
+        drawWall();
+        console::draw(3,3,"Hello");
+         if (console::key(console::K_ESC))
+        {
+            break;
+        }
+       
+        console::wait();
+       
+    }
+}
+
+void drawWall()
+{
+    for (int i = 0; i < BOARD_SIZE +1 ; i++)
+    {
+        for(int j = 0; j < BOARD_SIZE +1 ; j++)
+        {
+            if(i == 0 && j == 0)
+                console::draw(i,j ,WALL_LEFT_TOP_STRING);
+
+            else if ( i == BOARD_SIZE && j == 0)
+                console::draw(i,j , WALL_RIGHT_TOP_STRING);
+
+            else if( i == 0 && j== BOARD_SIZE)
+                console::draw(i,j, WALL_LEFT_BOTTOM_STRING);
+
+            else if(i == BOARD_SIZE && j == BOARD_SIZE)
+                console::draw(i,j, WALL_RIGHT_BOTTOM_STRING);
+
+            else if( (i>0 && i<BOARD_SIZE) && (j == 0 || j==BOARD_SIZE ))
+                console::draw(i,j, WALL_HORIZONTAL_STRING);
+
+            else if( (i == 0 || i == BOARD_SIZE) && (j >0 && j<BOARD_SIZE))
+                console::draw(i,j, WALL_VERTICAL_STRING);   
+
+            
+        }
+    }
 }
